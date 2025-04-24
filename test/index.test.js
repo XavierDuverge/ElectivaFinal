3a3975f27986132012bc6f5c4f7c8ca7bcd54c7d
XavierDuverge <xavierduvergemontero2@gmail.com>
2025-04-24 11:24:17 -0400

    Agregando pruebas unitarias exitosas y CI

--- test/index.test.js
+++ test/index.test.js
@@ -3,8 +3,9 @@ const path = require('path');
 const { JSDOM } = require('jsdom');
 
 test('La página contiene un h1 con "Hola Mundo"', () => {
-    const html = fs.readFileSync(path.resolve(__dirname, '../index.html'), 'utf8');
-    const dom = new JSDOM(html);
-    const h1 = dom.window.document.querySelector('h1');
-    expect(h1.textContent).toBe('Hola Mundo');
+  const html = fs.readFileSync(path.resolve(__dirname, '../index.html'), 'utf8');
+  const dom = new JSDOM(html);
+  const h1 = dom.window.document.querySelector('h1');
+
+  expect(h1.textContent).toContain('Hola Mundo');
 });
